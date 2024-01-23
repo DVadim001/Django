a331305bc3320082582a185875ca6a129844d57f
DVadim001 <dvadim001@gmail.com>
2024-01-23 20:32:11 +0500

    e_shop

---
+++ lambda/e_shop/index/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from . import forms
+# Create your views here.
+
+
+# отображение главной статраницы
+def home(request):
+    search_bar = forms.SearchForm()
+    # отправить элементы нафронт
+    context = {'form':search_bar}
+    return render(request, 'home.html', context)
+
+# отображение страницы о нас
+def about(request):
+    return render(request, 'about.html')
+
+
+# отображение страницы с контактами
+def about(request):
+    return render(request, 'contact.html')
