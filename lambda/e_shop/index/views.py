c4d029b79c4636200f5545f27027db860ce5c531
DVadim001 <dvadim001@gmail.com>
2024-01-26 12:17:03 +0500

    django_add models

--- lambda/e_shop/index/views.py
+++ lambda/e_shop/index/views.py
@@ -1,15 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from . import forms
+from .models import Product, Category, Cart
 # Create your views here.
 
 
 # отображение главной статраницы
 def home(request):
+    # Поисковая строка
     search_bar = forms.SearchForm()
-    # отправить элементы нафронт
-    context = {'form':search_bar}
+    # Собираем все продукты
+    product_info = Product.objects.all()
+    # Собираем все категории товаров
+    category_info = Category.objects.all()
+    # отправить элементы на фронт
+    context = {'form':search_bar,
+               'product': product_info,
+               'category': category_info}
     return render(request, 'home.html', context)
 
+
+# Вывод товаров по определённой категории
+def get_full_category(request, pk):
+    category = Category.objects.get(id=pk)
+    products = Product.objects.filter(category_name=category)
+    # Отправляем данные на фронт
+    context = {'products': products}
+    return render(request, 'category.html', context)
+
+
+# Вывод информации о конкретном продукте
+def get_full_product(request, pk):
+    product = Product.objects.get(id=pk)
+    context = {'product': product}
+    return render(request, 'product.html', context)
 
 # отображение страницы о нас
 def about(request):
@@ -24,4 +47,16 @@ def contact(request):
 # поиск продукта
 def search_product(request):
     if request.method == 'POST':
-        pass
+        get_product = request.POST.get('search_product')
+
+        try:
+            exat_product = Product.objects.get(product_name__icontains=get_product)
+
+            return redirect(f"pruduct/{exat_product.id}")
+        except:
+            return redirect('product-not-found')
+
+
+# Если продукт не был найден
+def pr_not_found(request):
+    return render(request, 'not_found.html')
